ffcece7cb10bf91bad2a317479b18f7eae68e11c
Filipe Lemos <filipe_lemos@live.com>
2023-02-24 17:50:21 -0300

    Adicionando e aplicando Lombok ao projeto

--- src/main/java/com/github/FilipeRobot/gerenciarPessoas/models/Endereco.java
+++ src/main/java/com/github/FilipeRobot/gerenciarPessoas/models/Endereco.java
@@ -2,14 +2,17 @@ package com.github.FilipeRobot.gerenciarPessoas.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  * Uma classe que representa uma entidade/tablea "endereco" dentro do banco de dados
  */
+@Data @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "endereco")
 public class Endereco implements Serializable {
@@ -36,98 +39,4 @@ public class Endereco implements Serializable {
     @ManyToOne
     @JoinColumn(name = "pessoa_id")
     private Pessoa pessoa;
-
-    public Endereco() {
-    }
-
-    public Endereco(Long id, String logradouro, String cep, Integer numero, String cidade, boolean enderecoPrincipal,
-                    Pessoa pessoa) {
-        super();
-        this.id = id;
-        this.logradouro = logradouro;
-        this.cep = cep;
-        this.numero = numero;
-        this.cidade = cidade;
-        this.enderecoPrincipal = enderecoPrincipal;
-        this.pessoa = pessoa;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogradouro() {
-        return logradouro;
-    }
-
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public boolean isEnderecoPrincipal() {
-        return enderecoPrincipal;
-    }
-
-    public void setEnderecoPrincipal(boolean enderecoPrincipal) {
-        this.enderecoPrincipal = enderecoPrincipal;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Endereco other = (Endereco) obj;
-        return id == other.id;
-    }
-
-    @Override
-    public String toString() {
-        return "Endereco [id=" + id + ", logradouro=" + logradouro + ", cep=" + cep + ", numero=" + numero + ", cidade="
-                + cidade + ", enderecoPrincipal=" + enderecoPrincipal + ", pessoa=" + pessoa + "]";
-    }
-
-    public Pessoa getPessoa() {
-        return pessoa;
-    }
-
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
-    }
 }
--- src/main/java/com/github/FilipeRobot/gerenciarPessoas/models/Pessoa.java
+++ src/main/java/com/github/FilipeRobot/gerenciarPessoas/models/Pessoa.java
@@ -1,17 +1,20 @@
 package com.github.FilipeRobot.gerenciarPessoas.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Uma classe que representa uma entidade/tablea "pessoa" dentro do banco de dados
  */
+@Data @AllArgsConstructor @NoArgsConstructor
 @Entity
 @Table(name = "pessoa")
 public class Pessoa implements Serializable {
@@ -30,70 +33,4 @@ public class Pessoa implements Serializable {
 
     @OneToMany(mappedBy = "pessoa")
     private List<Endereco> endereco;
-
-    public Pessoa() {
-    }
-
-    public Pessoa(UUID id, String nome, LocalDate dataDeNascimento) {
-        super();
-        this.id = id;
-        this.nome = nome;
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-    public LocalDate getDataDeNascimento() {
-        return dataDeNascimento;
-    }
-
-    public void setDataDeNascimento(LocalDate dataDeNascimento) {
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Pessoa other = (Pessoa) obj;
-        return Objects.equals(id, other.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Pessoa [id=" + id + ", nome=" + nome + ", dataDeNascimento=" + dataDeNascimento + ", endereco="
-                + endereco + "]";
-    }
-
-    public List<Endereco> getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(List<Endereco> endereco) {
-        this.endereco = endereco;
-    }
 }
